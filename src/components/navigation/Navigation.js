771e60f01f4d20b1505ba477111f583b7f8fb84a
Dennis <dmiller1623@gmail.com>
2018-11-15 09:16:19 -0700

    Add backgrounds and styles

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -18,15 +18,25 @@ class Navigation extends Component {
   }
 
   render() {
+    // if(this.state.view === 'projects')
     return (
-      <div className='navigation-section'>
-        <div className='github-section'>
-          <h1 className='button-title'>Gituhub</h1>
-          <img className='github-image' src={github}/>
+      <div className='navigation-container'>
+        <div className='heading-section'>
+          <h1 className='heading-name'>Dennis Miller</h1>
+          <h1 className='heading-title'>FrontEnd Developer</h1>
+          <img className='heading-image' src='https://sg.fiverrcdn.com/photo2s/115792331/original/24e73b206afe0be4da2b941ab06bb09efe2a10a0.jpeg?1534395867'/>
         </div>
-        <div className='projects-section'>
-          <h1 className='button-title'>Projects</h1>
-          <img className='projects-image' src={folder}/>
+        <div className='navigation-section'>
+          <div className='github-section'>
+            <h1 className='button-title'>Github</h1>
+            <a href='https://github.com/dmiller1623'>
+              <img className='github-image' src={github}/>
+            </a>
+          </div>
+          <div className='projects-section'>
+            <h1 className='button-title'>Projects</h1>
+            <img className='projects-image' src={folder}/>
+          </div>
         </div>
       </div>
     )
