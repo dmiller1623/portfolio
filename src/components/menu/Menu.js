0cdeae6eecd575500a703fcb62032fc76c732917
Dennis <dmiller1623@gmail.com>
2018-11-15 17:20:36 -0700

    Add transitions for the icons

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import github from '../../images/github-link.png'
 import folder from '../../images/folder.png'
 import menu from '../../images/menu.svg'
+import bio from '../../images/user.svg'
 import './Menu.css'
 
 class Menu extends Component {
   constructor() {
     super();
     this.state = {
-      active: ''
+      active: '',
+      hover: ''
     }
+  }
+
+  handleHover = (event) => {
+    switch(event.target.className) {
+      case ('github-image'): 
+        this.setState({
+          hover: 'Github'
+        })
+      break;
+      case ('projects-image'):
+        this.setState({
+          hover: 'Projects'
+        })
+      break;
+      case ('bio-image'):
+      this.setState({
+        hover: 'About Me'
+      })
+    break;
+      default: 
+        this.setState({
+          hover: ''
+        })
+    }
+  }
+
+  handleExitHover = () => {
+    this.setState({
+      hover: ''
+    })
   }
 
   handleActive = () => {
@@ -26,20 +57,6 @@ class Menu extends Component {
   }
 
   render() {
-    if(!this.state.active) {
-      return (
-        <div className='menu-container'>
-           <div className='heading-section'>
-            <h1 className='heading-name'>Dennis Miller</h1>
-            <h1 className='heading-title'>FrontEnd Developer</h1>
-            <img className='heading-image' src='https://sg.fiverrcdn.com/photo2s/115792331/original/24e73b206afe0be4da2b941ab06bb09efe2a10a0.jpeg?1534395867'/>
-          </div>
-          <div className='navigation-section'>
-           <img className='menu-button' src={menu} onClick={this.handleActive}/>
-          </div>
-        </div>
-      )
-    }
     return (
       <div className='menu-container'>
         <div className='heading-section'>
@@ -47,23 +64,36 @@ class Menu extends Component {
           <h1 className='heading-title'>FrontEnd Developer</h1>
           <img className='heading-image' src='https://sg.fiverrcdn.com/photo2s/115792331/original/24e73b206afe0be4da2b941ab06bb09efe2a10a0.jpeg?1534395867'/>
         </div>
-          <div className='navigation-section'>
-            {/* <div className='menu-button'> */}
-              <ul className={this.state.active}>
-                {/* <div> */}
-                  <img className='menu-button' src={menu} onClick={this.handleActive}/>
-                  <a className='github-image' href='https://github.com/dmiller1623'></a>
-                  <img onClick={() => this.props.handleView('projects')} className='projects-image' src={folder}/> */}
-                {/* </div> */}
-              </ul>
-            {/* </div> */}
-            {/* <div className='github-section'> */}
-              {/* <h1 className='button-title'>Github</h1>
-            </div>
-            <div className='projects-section'>
-              <h1 className='button-title'>Projects</h1> */}
-            {/* </div> */}
+        <div className='navigation-section'>
+          <div className={this.state.active ? this.state.active : 'not-active'}>
+            <img 
+              className='menu-button' 
+              src={menu} 
+              onClick={this.handleActive}
+            />
+            <a 
+              className='github-image' 
+              href='https://github.com/dmiller1623'
+              onMouseOver={(event) => this.handleHover(event)}
+              onMouseLeave={this.handleExitHover}
+              >
+            </a>
+            <img 
+              className='projects-image' 
+              src={folder}
+              onClick={() => this.props.handleView('projects')}
+              onMouseOver={(event) => this.handleHover(event)}
+              onMouseLeave={this.handleExitHover} 
+            />
+            <img
+              className='bio-image'
+              src={bio}
+              onMouseOver={(event) => this.handleHover(event)}
+              onMouseLeave={this.handleExitHover}
+            />
+          <h1 className='hover-heading'>{this.state.hover}</h1>
           </div>
+        </div>
       </div>
     )
   }
