1287e0c82066f3fd83f9242ab6311f41ecba8541
Dennis <dmiller1623@gmail.com>
2018-12-18 12:19:06 -0700

    Add download for resume

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './AboutMe.css'
 import denver from '../../images/denver.jpg'
 import profileImage from '../../images/img.png'
+import resume from '../../files/resume.pdf'
 
 class AboutMe extends Component {
   constructor() {
@@ -16,7 +17,9 @@ class AboutMe extends Component {
       <div className='about'>
         <img className='denver-image' src={denver}/>
         <div className='about-container'>
-          <section className='about-section left first'></section>
+          <section className='about-section left first'>
+            <a href={resume} download={resume}>Download</a>
+          </section>
           <section className='about-section right first'></section>
         </div>
         <div className='about-container'>
